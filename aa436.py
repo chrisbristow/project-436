39a3c637cb3d3554da90be10f2cdf211a70633fc
Chris Bristow <cmichaelbristow@gmail.com>
2013-03-18 20:47:57 +0000

    Initial commit.

---
+++ aa436.py
@@ -0,0 +1,539 @@
+import sys
+import os
+import time
+import re
+import select
+import string
+import subprocess
+import logging
+import logging.handlers
+from socket import *
+
+
+
+
+# Globals:
+
+file_consumer_list = []
+host_name = os.uname()[1]
+ps_command = []
+process_list = []
+uid_seed = 0
+alert_queue = []
+cmd_list = []
+logger = logging.getLogger(__name__)
+
+
+
+
+# Return True if current time is within range
+# given in active_string.
+
+def is_active(active_string):
+  if(len(active_string) == 0):
+    actv = True
+
+  else:
+    actv = False
+    ltm = time.localtime()
+
+    for t in active_string.split(','):
+      tmt = re.match('^([0-9]+);(\d+):(\d+)\-(\d+):(\d+)$', t)
+
+      if tmt:
+        if(((ltm[3] * 60) + ltm[4]) >= ((int(tmt.group(2)) * 60) + int(tmt.group(3))) and ((ltm[3] * 60) + ltm[4]) <= ((int(tmt.group(4)
+) * 60) + int(tmt.group(5)))):
+          if(string.find(tmt.group(1), str(ltm[6])) > -1):
+            actv = True
+
+  return(actv)
+
+
+
+# An instance of file_consumer is created for each file
+# tracking configuration.
+
+class file_consumer:
+  def __init__(self, filename, matches, actions):
+    global logger
+
+    self.open = False
+    self.filename = filename
+    self.seek = 2
+    self.matches = matches
+    self.tags = actions['tags']
+    self.message = ''
+    self.active = ''
+
+    logger.info('Creating file consumer for file '+filename+' (patterns: '+str(matches)+', '+str(actions)+')')
+
+    if('message' in actions):
+      self.message = actions['message']
+
+    self.period = 0
+
+    if('period' in actions):
+      self.period = int(actions['period'])
+
+    self.threshold = 0
+
+    if('threshold' in actions):
+      self.threshold = int(actions['threshold'])
+
+    self.count = 0
+    self.next_report = time.time() + self.period
+
+    self.metric = 0
+
+    if('metric' in actions):
+      self.metric = int(actions['metric'])
+
+    if('active' in actions):
+      self.active = actions['active']
+
+
+
+
+  def __del__(self):
+    self.fd.close()
+    logger.info('Removing file consumer for file '+self.filename)
+
+
+
+  # Checks to see if any of the "periodic" (eg. activity, metric) file events
+  # need to be raised.
+
+  def check_period(self):
+    if(self.period > 0):
+      if(time.time() > self.next_report):
+
+        # Alert if matches exceed the threshold.
+        if(self.threshold > 0 and self.count > self.threshold and len(self.message) > 0):
+          self.list = self.list + [ self.tags + '%%' + self.filename + '%%' + self.message ]
+
+        # Alert if no matches within n seconds.
+        elif(self.count == 0 and len(self.message) > 0 and self.threshold == 0):
+          self.list = self.list + [ self.tags + '%%' + self.filename + '%%' + self.message ]
+
+        # Output the count of matches every n seconds.
+        elif(len(self.message) == 0):
+          self.list = self.list + [ self.tags + '%%' + self.filename + '%%' + str(self.count) ]
+
+        self.next_report = time.time() + self.period
+        self.count = 0
+
+
+
+
+  # The main program calls read() for each file tracker.  Returns
+  # a list of events from the do_read() function if within an active
+  # time.
+
+  def read(self):
+    if(is_active(self.active) == True):
+      return(self.do_read())
+
+    else:
+      return([])
+
+
+
+
+  # Read from files.
+
+  def do_read(self):
+    global logger
+
+    self.list = []
+
+    self.check_period()
+
+    self.finished = False
+
+    while(self.finished == False):
+      if(self.open == False):
+        try:
+          self.st = os.stat(self.filename)
+          self.inode = self.st.st_ino
+          self.fd = open(self.filename)
+          self.fd.seek(0, self.seek)
+          self.open = True
+          self.seek = 0
+  
+        except Exception:
+          logger.error('Error: File '+self.filename+' not found')
+          self.finished = True
+  
+      else:
+        self.cp = self.fd.tell()
+        self.nextline = self.fd.readline()
+  
+        if not self.nextline:
+          try:
+            self.st = os.stat(self.filename)
+     
+            if(self.st.st_ino != self.inode):
+              self.fd.close()
+              self.open = False
+            else:
+              self.fd.seek(self.cp)
+              self.finished = True
+  
+          except Exception:
+            logger.error('Error: File '+self.filename+' not found')
+            self.finished = True
+  
+        else:
+          for m in self.matches:
+            if(re.search(m,self.nextline) is not None):
+              if(self.period > 0):
+                self.count += 1
+
+              elif(len(self.message) > 0):
+                # Alert every match with a pre-defined message.
+                self.list = self.list + [ self.tags + '%%' + self.filename + '%%' + self.message ]
+
+              else:
+                # Alert every match with the actual line matched.
+                self.list = self.list + [ self.tags + '%%' + self.filename + '%%' + self.nextline.strip() ]
+
+    return(self.list)
+
+
+
+
+# Process configuration from ax436.
+
+def do_config(conf):
+  global file_consumer_list
+  global ps_command
+  global process_list
+  global cmd_list
+  global logger
+
+  c_file = ''
+  c_match = []
+  c_active = ''
+  c_process = ''
+
+  logger.info('Configuration received from server')
+
+  for cl in string.split(conf,'%%'):
+    m = re.match('^([a-z_:]+)\s+(.+)\s*$', cl)
+
+    if m:
+      cmd = m.group(1)
+      arg = m.group(2)
+
+      if(cmd == 'file:'):
+        c_file = arg
+
+      elif(cmd == 'match:'):
+        c_match += [ arg ]
+
+      elif(cmd == 'active:'):
+        c_active = arg
+
+      elif(cmd == 'alert_all:' and len(c_match) > 0 and len(c_file) > 0):
+        am = re.match('^tags=(\S+)\s+message=(.+)\s*$', arg)
+
+        if am:
+          file_consumer_list += [ file_consumer(c_file, c_match, { 'tags': am.group(1), 'message': am.group(2), 'active': c_active }) ]
+
+        else:
+          am2 = re.match('^tags=(\S+)\s*$', arg)
+
+          if am2:
+            file_consumer_list += [ file_consumer(c_file, c_match, { 'tags': am2.group(1), 'active': c_active }) ]
+
+        c_file = ''
+        c_match = []
+        c_active = ''
+
+      elif(cmd == 'alert_n:' and len(c_match) > 0 and len(c_file) > 0):
+        am = re.match('^tags=(\S+)\s+threshold=(\d+)\s+seconds=(\d+)\s+message=(.+)\s*$', arg)
+
+        if am:
+          file_consumer_list += [ file_consumer(c_file, c_match, { 'tags': am.group(1), 'threshold': am.group(2), 'period': am.group(3), 'message': am.group(4), 'active': c_active }) ]
+
+        c_file = ''
+        c_match = []
+        c_active = ''
+
+      elif(cmd == 'alert_count:' and len(c_match) > 0 and len(c_file) > 0):
+        am = re.match('^tags=(\S+)\s+seconds=(\d+)\s*$', arg)
+
+        if am:
+          file_consumer_list += [ file_consumer(c_file, c_match, { 'tags': am.group(1), 'period': am.group(2), 'metric': '1', 'active': c_active }) ]
+
+        c_file = ''
+        c_match = []
+        c_active = ''
+
+      elif(cmd == 'alert_inactive:' and len(c_match) > 0 and len(c_file) > 0):
+        am = re.match('^tags=(\S+)\s+seconds=(\d+)\s+message=(.+)\s*$', arg)
+
+        if am:
+          file_consumer_list += [ file_consumer(c_file, c_match, { 'tags': am.group(1), 'period': am.group(2), 'message': am.group(3), 'metric': '2', 'active': c_active }) ]
+
+        c_file = ''
+        c_match = []
+        c_active = ''
+
+      elif(cmd == 'ps_command:'):
+        ps_command = arg.split()
+        logger.info('Process check command: '+str(ps_command))
+
+      elif(cmd == 'process:'):
+        c_process = arg
+
+      elif(cmd == 'alert_running:' and len(c_process) > 0):
+        pc = re.match('^tags=(\S+)\s+min=(\d+)\s+max=(\d+)\s+message=(.+)\s*$', arg)
+
+        if pc:
+          pc_rec = { 'match': c_process, 'tags': pc.group(1), 'min': int(pc.group(2)), 'max': int(pc.group(3)), 'message': pc.group(4), 'count': 0, 'error': 0, 'active': c_active }
+          process_list += [ pc_rec ]
+          logger.info('Watching process: '+str(pc_rec))
+
+        c_process = ''
+        c_active = ''
+
+      elif(cmd == 'run:'):
+        cm = re.match('^command=(.+)\s+extract=(.+)\s*$', arg)
+
+        if cm:
+          new_cmd_list = { 'command': cm.group(1).strip().split(), 'extract': cm.group(2).strip(), 'alerts': [] }
+          logger.info('Running command: '+str(new_cmd_list))
+          cmd_list.append(new_cmd_list)
+
+      elif(cmd == 'alert_if:'):
+        cm = re.match('^tags=(\S+)\s+match=(\d+),(\S+)\s+upper_limit=(\d+),([0-9\.]+)\s+message=(.+)\s*$', arg)
+
+        if cm:
+          new_cmd_alist = { 'tags': cm.group(1), 'match_n': int(cm.group(2)), 'match_str': cm.group(3), 'upper_limit_n': int(cm.group(4)), 'upper_limit_v': float(cm.group(5)), 'message': cm.group(6), 'active': c_active }
+          logger.info('Alerting on command output: '+str(new_cmd_alist))
+          cmd_list[len(cmd_list)-1]['alerts'].append(new_cmd_alist)
+          c_active = ''
+
+        cm = re.match('^tags=(\S+)\s+match=(\d+),(\S+)\s+lower_limit=(\d+),([0-9\.]+)\s+message=(.+)\s*$', arg)
+
+        if cm:
+          new_cmd_alist = { 'tags': cm.group(1), 'match_n': int(cm.group(2)), 'match_str': cm.group(3), 'lower_limit_n': int(cm.group(4)), 'lower_limit_v': float(cm.group(5)), 'message': cm.group(6), 'active': c_active }
+          logger.info('Alerting on command output: '+str(new_cmd_alist))
+          cmd_list[len(cmd_list)-1]['alerts'].append(new_cmd_alist)
+          c_active = ''
+
+      elif(cmd == 'alert_metric:'):
+        cm = re.match('^tags=(\S+)\s+match=(\d+),(\S+)\s+metric=(\d+)\s*$', arg)
+
+        if cm:
+          new_cmd_alist = { 'tags': cm.group(1), 'match_n': int(cm.group(2)), 'match_str': cm.group(3), 'metric': int(cm.group(4)), 'active': c_active }
+          logger.info('Publishing on command output: '+str(new_cmd_alist))
+          cmd_list[len(cmd_list)-1]['alerts'].append(new_cmd_alist)
+          c_active = ''
+
+
+
+
+
+# Reset all configuration
+
+def do_unconfig():
+  global logger
+  global file_consumer_list
+  global ps_command
+  global process_list
+  global cmd_list
+
+  logger.info('Unconfiguring')
+
+  file_consumer_list = []
+  ps_command = []
+  process_list = []
+  cmd_list = []
+
+
+
+
+
+# Add an alert to the alert queue.  Assigns a unique (for this host)
+# alert id.
+
+def queue_alert(alert):
+  global uid_seed
+  global alert_queue
+  global logger
+
+  if(len(alert_queue) < 256):
+    logger.info(time.ctime()+' Queueing: '+alert)
+
+    uid = '{0}_{1}'.format(int(time.time()), uid_seed)
+    uid_seed += 1
+    alert_queue += [ [ uid, alert, 0, int(time.time()) ] ]
+
+  else:
+    logger.info(time.ctime()+' Alert not queued (queue full): '+alert)
+
+
+
+
+# Main program loop.
+
+def main(port):
+  global process_list
+  global alert_queue
+  global logger
+  global host_name
+  global cmd_list
+
+  logger.setLevel(logging.DEBUG)
+  logger.addHandler(logging.handlers.RotatingFileHandler('aa436.log', maxBytes = 1000000, backupCount = 4))
+
+  logger.info(time.ctime())
+  logger.info('Listening on port '+str(port))
+  logger.info('Server on port '+str(port + 1))
+
+  server_name = ''
+  server_seen = 0
+  server_seen_timeout = 30
+  configured = False
+  last_config_req = 0
+  last_update = time.time()
+  idle_time = 67
+  alert_queue = []
+  process_check_interval = 20
+  next_process_check = int(time.time()) + process_check_interval
+  stats_check_interval = 60
+  next_stats_check = int(time.time()) + stats_check_interval
+  last_process_event = 0
+
+  addr = ('', port)
+  ad_sock = socket(AF_INET, SOCK_DGRAM)
+  ad_sock.bind(addr)
+
+  inputs = [ ad_sock ]
+  outputs = []
+
+  while(True):
+    readable, writable, exceptional = select.select(inputs, outputs, inputs, 1.0)
+
+    for rs in readable:
+      udp_data = rs.recv(65536)
+      m = re.match('^([A-Z]+)%%(.+)', udp_data)
+
+      if m:
+        cmd = m.group(1)
+        arg = m.group(2)
+
+        if(cmd == 'SRVHB'):
+          if(arg == server_name):
+            server_seen = time.time()
+
+          if(len(server_name) == 0):
+            server_name = arg
+            logger.info('Selected server: '+arg)
+
+        elif(cmd == 'CONFIG'):
+          do_config(arg)
+          configured = True
+
+        elif(cmd == 'RESET'):
+          if(arg == host_name):
+            do_unconfig()
+            last_config_req = time.time() + 10
+            configured = False
+
+        elif(cmd == 'ACK'):
+          if(arg == alert_queue[0][0]):
+            del alert_queue[0]
+
+    if(server_seen > 0 and time.time() > (server_seen + server_seen_timeout)):
+      logger.info('Deselected server: '+server_name)
+      server_name = ''
+      server_seen = 0
+
+    if(configured == False and len(server_name) > 0 and time.time() > (last_config_req + 10)):
+      last_config_req = time.time()
+      logger.info('Requesting configuration from '+server_name+' on port '+str(port+1))
+      ad_sock.sendto('CONFREQ%%'+os.uname()[1], (server_name, port+1))
+      last_update = time.time()
+
+    if(server_seen > 0 and time.time() > (last_update + idle_time) and len(alert_queue) < 3):
+      queue_alert('SYSTEM%%NULL%%Idle')
+      last_update = time.time()
+
+    for fc in file_consumer_list:
+      for alert in fc.read():
+        queue_alert(alert)
+
+    if(len(ps_command) > 0 and int(time.time()) > next_process_check):
+      ps_output = subprocess.check_output(ps_command)
+
+      for zeroing_idx in range(len(process_list)):
+        process_list[zeroing_idx]['count'] = 0
+
+      for ps_line in string.split(ps_output, '\n'):
+        for watching_idx in range(len(process_list)):
+          pm = re.search(process_list[watching_idx]['match'], ps_line)
+
+          if pm:
+            process_list[watching_idx]['count'] += 1
+
+      for checking_idx in range(len(process_list)):
+        if(process_list[checking_idx]['count'] < process_list[checking_idx]['min'] or process_list[checking_idx]['count'] > process_list[checking_idx]['max']):
+          process_list[checking_idx]['error'] += 1
+
+        else:
+          process_list[checking_idx]['error'] = 0
+
+        if(process_list[checking_idx]['error'] > 1 and is_active(process_list[checking_idx]['active']) == True):
+          process_alert_msg = process_list[checking_idx]['tags'] + '%%NULL%%' + process_list[checking_idx]['message'] + ' [' + str(process_list[checking_idx]['count']) + ']'
+          queue_alert(process_alert_msg)
+          process_list[checking_idx]['error'] = 0
+          last_process_event = int(time.time())
+
+      next_process_check = int(time.time()) + process_check_interval
+
+    if(time.time() > next_stats_check):
+      for run_cmd in cmd_list:
+        for cmd_output_line in string.split(subprocess.check_output(run_cmd['command']), '\n'):
+          c_ext = re.search(run_cmd['extract'], cmd_output_line)
+
+          if c_ext:
+            for run_alist in run_cmd['alerts']:
+              if('upper_limit_n' in run_alist and is_active(run_alist['active'])):
+                if(run_alist['match_str'] == c_ext.group(run_alist['match_n']) and float(c_ext.group(run_alist['upper_limit_n'])) > run_alist['upper_limit_v']):
+                  queue_alert(run_alist['tags']+'%%NULL%%'+run_alist['message'])
+
+              elif('lower_limit_n' in run_alist and is_active(run_alist['active'])):
+                if(run_alist['match_str'] == c_ext.group(run_alist['match_n']) and float(c_ext.group(run_alist['lower_limit_n'])) < run_alist['lower_limit_v']):
+                  queue_alert(run_alist['tags']+'%%NULL%%'+run_alist['message'])
+
+              elif('metric' in run_alist and is_active(run_alist['active'])):
+                if(run_alist['match_str'] == c_ext.group(run_alist['match_n'])):
+                  queue_alert(run_alist['tags']+'%%NULL%%'+str(c_ext.group(run_alist['metric'])))
+
+      next_stats_check = int(time.time()) + stats_check_interval
+
+    if(last_process_event > 0 and time.time() > (last_process_event + (process_check_interval * 2) + 30)):
+      last_process_event = 0
+      queue_alert('SYSTEM%%NULL%%Process check: All clear')
+
+    if(len(alert_queue) > 0):
+      if(int(time.time()) > alert_queue[0][2]):
+        ad_sock.sendto('ALERT%%'+host_name+'%%'+alert_queue[0][0]+'%%'+str(alert_queue[0][3])+'%%'+alert_queue[0][1], (server_name, port+1))
+        alert_queue[0][2] = int(time.time()) + 10
+        last_update = time.time()
+
+
+
+
+# Launch from here.
+
+if __name__ == '__main__':
+  if len(sys.argv) < 2:
+    print 'Usage: aa436.py udp_port'
+    exit(1)
+
+  else:
+    main(int(sys.argv[1]))
---
+++ ax436.py
@@ -0,0 +1,200 @@
+# ax436.py
+#
+# This is the Server for Project 436.  It's main functions are:
+# - Broadcasting "I am here" notifications to the local LAN so that
+#   Project 436 Agents (aa436.py) can find it.
+# - Supplying Agents with their configurations, on demand.
+# - Sending Agents "Reset" commands if their configuration files
+#   are updated.
+# - Checking whether configured Agents have supplied a heartbeat
+#   notification back to the Server recently.
+# - Capture and acknowledgement of fault / metric events from
+#   Agents.
+
+
+
+import sys
+import os
+import time
+import re
+import select
+import string
+import logging
+import logging.handlers
+from socket import *
+
+
+
+
+# Main program.  The Server initialises it's log files (one for
+# general log items and the other for it's Event Stream), reads
+# and parses it's configuration file, sets up it's various UDP
+# sockets and then enters a loop.  The loop processes commands
+# and events from Agents as well as monitoring Agent configuration
+# files - if a configuration file is updated, then the Agent
+# the file refers to is notified ("Reset") so that it can
+# re-initialise itself.
+
+def main(conf_file):
+  # This is the time after which an Agent is considered
+  # "dead" if that Agent doesn't send any notifications
+  # to the Server.
+  max_idle_time = 180
+
+  # Initialise the general Server log - ax436.log.
+  logger = logging.getLogger(__name__)
+  logger.setLevel(logging.DEBUG)
+  logger.addHandler(logging.handlers.RotatingFileHandler('ax436.log', maxBytes = 1000000, backupCount = 4))
+
+  logger.info(time.ctime())
+
+  # Read and parse the Server's configuration file.
+  f = open(conf_file)
+
+  for cf in f:
+    if cf.startswith('port:'):
+      tokens = cf.split()
+      port = int(tokens[1])
+
+    elif cf.startswith('broadcast:'):
+      tokens = cf.split()
+      bcaddr = tokens[1]
+
+    elif cf.startswith('hosts:'):
+      tokens = cf.split()
+      host_dir = tokens[1]
+
+    elif cf.startswith('includes:'):
+      tokens = cf.split()
+      include_dir = tokens[1]
+
+    elif cf.startswith('event_stream:'):
+      tokens = cf.split()
+      event_stream_name = tokens[1]
+
+  f.close()
+
+  logger.info('Server broadcasting on port '+str(port)+' to '+bcaddr)
+  logger.info('Agent channel on port '+str(port + 1))
+  logger.info('Host configurations in '+host_dir)
+  logger.info('Additional configurations in '+include_dir)
+  logger.info('Event stream is '+event_stream_name)
+
+  # Initialise the Server's event stream log file.
+  event_logger = logging.getLogger('ax436_event_logger')
+  event_logger.setLevel(logging.DEBUG)
+  event_logger.addHandler(logging.handlers.RotatingFileHandler(event_stream_name, maxBytes = 1000000, backupCount = 4))
+
+  addr = ('', port + 1)
+  send_addr = (bcaddr, port)
+
+  ad_sock = socket(AF_INET, SOCK_DGRAM)
+  ad_sock.bind(addr)
+  ad_sock.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
+
+  inputs = [ ad_sock ]
+  outputs = []
+
+  # The Agent configuration folder is scanned every [scan_interval] seconds
+  # to check for Agent configuration file changes.
+  scan_interval = 10
+  next_scan = time.time() + scan_interval
+
+  # "I am here" broadcasts are sent every [i_am_here] seconds.
+  i_am_here_interval = 5
+  next_i_am_here = time.time() + i_am_here_interval
+
+  scan_hash = {}
+
+  while(True):
+    readable, writable, exceptional = select.select(inputs, outputs, inputs, 1.0)
+
+    for rs in readable:
+      ( udp_data, from_addr ) = rs.recvfrom(65536)
+      m = re.match('^([A-Z]+)%%(.+)', udp_data)
+
+      if m:
+        cmd = m.group(1)
+        arg = m.group(2)
+
+        # Respond to a "Configuration Requested" command from an Agent.
+        if(cmd == 'CONFREQ'):
+          logger.info('Received configuration request from '+arg)
+
+          clines = 'START'
+
+          try:
+            logger.info('Loading host file '+host_dir+"/"+arg)
+
+            cfile = open(host_dir+"/"+arg)
+  
+            for c in cfile:
+              inc = re.match('^include:\s+(\S+)\s*$', c)
+  
+              if inc:
+                logger.info('Loading include file '+include_dir+"/"+inc.group(1))
+
+                ifile = open(include_dir+"/"+inc.group(1))
+  
+                for i in ifile:
+                  clines += '%%'+i.strip()
+  
+                ifile.close()
+  
+              else:
+                clines += '%%'+c.strip()
+  
+            cfile.close()
+
+            logger.info('Sending configuration for '+arg+' on port '+str(port))
+  
+            ad_sock.sendto('CONFIG%%'+clines, (from_addr[0], port))
+
+          except IOError:
+            logger.info('Error: Unable to return configuration for host '+arg)
+
+        # Respond to an Event sent to the Server by an Agent.
+        elif(cmd == 'ALERT'):
+          event_logger.info(time.ctime()+'%%'+arg)
+          ss = string.split(arg,'%%')
+          ad_sock.sendto('ACK%%'+ss[1], (from_addr[0], port))
+
+          if(ss[0] in scan_hash):
+            scan_hash[ss[0]]['agent_seen'] = int(time.time())
+
+    # Broadcast an "I am here" heartbeat message.
+    if(time.time() > next_i_am_here):
+      ad_sock.sendto('SRVHB%%'+os.uname()[1], send_addr)
+      next_i_am_here = time.time() + i_am_here_interval
+
+    # Scan the host configuration folder for changes.
+    if(time.time() > next_scan):
+      for sh in os.listdir(host_dir):
+        sst = os.stat(host_dir+'/'+sh)
+
+        if(sh not in scan_hash):
+          scan_hash[sh] = { 'agent_seen': 0, 'mtime': int(sst.st_mtime) }
+          logger.info('New host configuration: '+sh)
+
+        elif(scan_hash[sh]['mtime'] != int(sst.st_mtime)):
+          logger.info('Host configuration '+sh+' has been updated')
+          scan_hash[sh]['mtime'] = int(sst.st_mtime)
+          ad_sock.sendto('RESET%%'+sh, send_addr)
+
+        if((int(time.time()) - scan_hash[sh]['agent_seen']) > max_idle_time):
+          event_logger.info(time.ctime()+'%%'+sh+'%%000000%%'+str(int(time.time()))+'%%SYSTEM%%NULL%%Host is inactive')
+
+      next_scan = time.time() + scan_interval
+
+
+
+
+# Start hook.
+
+if __name__ == '__main__':
+  if len(sys.argv) < 2:
+    print 'Usage: ax436.py configuration_file'
+    exit(1)
+
+  else:
+    main(sys.argv[1])
